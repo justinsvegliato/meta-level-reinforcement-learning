0ebf215ac5ae85e9f06816409d4fdcf0df3e0255
Dylan Cope <dylanr.cope@gmail.com>
2022-09-21 19:28:35 -0700

    Initial implementation of custom rl runner class for experiments

--- mlrl/run.py
+++ mlrl/run.py
@@ -1,23 +1,54 @@
 from .config import RUNS_DIR
+from .utils.render_utils import create_policy_eval_video
 
 import json
 import time
-from typing import List, Union, Callable
+from typing import List, Union, Callable, Dict
 from pathlib import Path
 from collections import defaultdict
 
 import tensorflow as tf
+from tf_agents.agents import TFAgent
+from tf_agents.policy import TFPolicy
+from tf_agents.policies.random_tf_policy import RandomTFPolicy
+from tf_agents.environments.tf_environment import TFEnvironment
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+
 import numpy as np
-import pandas as pd
 import wandb
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-sns.set()
+def compute_return_stats(environment: TFEnvironment,
+                         policy: TFPolicy,
+                         num_episodes: int = 3,
+                         max_steps: int = 100) -> float:
+    """
+    Computes mean and standard deviation for returns of a policy on a given environment.
+    """
+
+    returns = []
+    for _ in range(num_episodes):
+
+        time_step = environment.reset()
+        episode_return = 0.0
+        n_steps = 0
+        while not time_step.is_last() and n_steps < max_steps:
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+            n_steps += 1
+
+        returns.append(episode_return)
+
+    returns = np.array(returns)
+
+    return returns.mean(), returns.std()
 
 
 def time_id():
+    """ Returns an id based on the current time. """
     return int(time.time() * 1e7)
 
 
@@ -27,86 +58,103 @@ class TrainingRun:
     """
 
     def __init__(self,
-                 model,
-                 train_ds,
-                 ds_info=None,
-                 loss_fn=None,
-                 val_ds=None,
-                 optimiser=None,
-                 lr=0.001,
+                 agent: TFAgent,
+                 environment: TFEnvironment,
+                 model: tf.keras.Model,
                  num_epochs=10,
-                 batches_per_epoch=-1,
-                 val_steps=-1,
-                 custom_training_step=None,
-                 do_analysis=True,
-                 custom_analyse=None,
-                 model_type='classifier',
-                 val_freq=1,
-                 metric_fns: List[Union[tf.keras.metrics.Metric, str, Callable]] = None,
+                 collect_steps_per_iteration=1,
+                 train_steps_per_epoch=1000,
+                 replay_buffer_max_length=100000,
+                 eval_env: TFEnvironment = None,
+                 num_eval_episodes=1,
+                 eval_steps=250,
+                 initial_collect_steps=500,
+                 metric_fns: Dict[str, Callable[[TFAgent, TFEnvironment], float]] = None,
                  callbacks: List[tf.keras.callbacks.Callback] = None,
                  run_dir=None,
-                 keras_fit=False,
                  end_wandb=True,
                  verbose=1,
-                 wandb_entity='dydi-analogies',
+                 wandb_entity='drcope',
                  wandb_project='project',
                  name='run'):
+        """
+        Creates a new training run.
+
+        Args:
+            agent: The agent to train
+            environment: The environment to train the agent in
+            model: The model being optimised
+            num_epochs: The number of epochs to train for. Each epoch is defined as a
+                given number of training steps followed by an evaluation of the agent.
+            collect_steps_per_iteration: The number of steps to collect data for each
+                training step
+            train_steps_per_epoch: The number of training steps per epoch
+            replay_buffer_max_length: The maximum length of the replay buffer
+            eval_env: The environment to evaluate the agent in. If None, the training
+                environment is used.
+            num_eval_episodes: The number of episodes to run when collecting evaluation 
+                statistics.
+            eval_steps: The maximum number of steps to run per episode when collecting 
+                evaluation statistics.
+            initial_collect_steps: The number of steps to collect with a random policy 
+                before training begins.
+            metric_fns: A dictionary of metric functions to use when evaluating the agent.
+                The keys are the names of the metrics and the values are the metric
+                functions. The metric functions should take an agent and an environment
+                and return a float.
+        """
 
         self.run_id = time_id()
         self.name = name
+        self.agent = agent
+        self.environment = environment
         self.model = model
-        self.ds_info = ds_info
-        self.epoch = 0
-        self.train_ds = train_ds.repeat()
-        self.val_freq = val_freq
-        self.val_ds = val_ds.repeat()
-        self.training_step = custom_training_step or tf.function(training_step)
-        self.optimiser = optimiser or tf.keras.optimizers.Adam(learning_rate=lr)
-        self.metric_fns = metric_fns or model.compiled_metrics or []
-        self.run_dir = run_dir or f'{RUNS_DIR}/{model.name}/{self.name}-{self.run_id}'
-        self.model_weights_dir = f'{self.run_dir}/model_weights'
-        self.model_type = model_type
-        self.do_analysis = do_analysis
-        self.custom_analyse = custom_analyse or None
-        self.loss_fn = loss_fn or model.compiled_loss
-        self.num_epochs = num_epochs
-        self.history = None
-        self.verbose = verbose
         
+        # Training parameters
+        self.train_steps_per_epoch = train_steps_per_epoch
+        self.eval_steps = eval_steps
+        self.num_eval_episodes = num_eval_episodes
+        self.initial_collect_steps = initial_collect_steps
+        self.collect_steps_per_iteration = collect_steps_per_iteration
+        
+        self.epoch = 0
+        self.eval_env = eval_env or environment
+
+        # Data tracking
+
+        self.metric_fns = metric_fns or dict()
+        self.run_dir = run_dir or f'{RUNS_DIR}/{agent.name}/{self.name}-{self.run_id}'
+        self.model_weights_dir = f'{self.run_dir}/model_weights'
+
+        self.num_epochs = num_epochs
+        self.verbose = verbose
+
         Path(self.run_dir).mkdir(parents=True)
         Path(self.model_weights_dir).mkdir(parents=True)
 
+        self.user_given_callbacks = callbacks or []
+        self.callbacks = None  # To be defined in pre execution setup due to wandb config
+
+        # Wandb variables
         self.end_wandb = end_wandb
         self.wandb_project = wandb_project
         self.wandb_entity = wandb_entity
 
-        if batches_per_epoch > 0:
-            self.batches_per_epoch = batches_per_epoch
-        else:
-            self.batches_per_epoch = len(train_ds)
+        # TF-agents variables
 
-        if val_steps > 0:
-            self.val_steps = val_steps
-        elif val_ds:
-            self.val_steps = len(val_ds)
-        else:
-            self.val_steps = None
+        self.replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
+            data_spec=agent.collect_data_spec,
+            batch_size=environment.batch_size,
+            max_length=replay_buffer_max_length)
 
-        self.keras_fit = keras_fit
-        self.model.compile(loss=loss_fn,
-                           optimizer=self.optimiser,
-                           metrics=metric_fns)
+        self.experience_dataset = None  # setup in pre-execution
+        self.experience_iterator = None  # setup in pre-execution
 
-        self.build()
-
-        self.user_given_callbacks = callbacks or []
-        self.callbacks = None  # To be defined in pre execution setup
-
-    def build(self):
-        print(f'Building model: {self.model}')
-        x, y_true = next(iter(self.train_ds))
-        y_pred = self.model(x, training=True)
-        self.loss_fn(y_true, y_pred)
+        self.random_policy = RandomTFPolicy(
+            environment.time_step_spec(),
+            environment.action_spec(),
+            observation_and_action_constraint_splitter=agent._observation_and_action_constraint_splitter
+        )
 
     def get_config(self):
         try:
@@ -127,94 +175,96 @@ class TrainingRun:
             'metrics': metrics,
             'ds_info': self.ds_info,
             'train_steps_per_epoch': self.batches_per_epoch,
-            # 'callback_params': self.callbacks.params,
         }
 
-    def update_model(self, inputs, targets):
+    def training_step(self) -> Dict[str, float]:
 
-        logs = self.training_step(self.model,
-                                  inputs,
-                                  targets,
-                                  self.optimiser,
-                                  self.loss_fn,
-                                  self.model.trainable_variables)
+        logs = dict()
 
-        if not isinstance(logs, dict):
-            logs = {'loss': logs}  # assume it is a loss value
+        # Collect a few steps using collect_policy and save to the replay buffer.
+        self.collect_data(self.agent.collect_policy, self.collect_steps_per_iteration)
+
+        # Sample a batch of data from the buffer and update the agent's network.
+        experience, _ = next(self.experience_iterator)
+        logs['train_loss'] = self.agent.train(experience).loss
+
         return logs
+
+    def get_evaluation_stats(self) -> Dict[str, float]:
+        logs = dict()
+        return_mean, return_std = compute_return_stats(
+            self.eval_env, self.agent.policy,
+            self.num_eval_episodes, self.eval_steps
+        )
+
+        logs['eval_return_mean'] = return_mean
+        logs['eval_return_std'] = return_std
+
+        logs.update({
+            metric: fn(self.agent, self.eval_env)
+            for metric, fn in self.metric_fns.items()
+        })
+
+        return logs
+
+    def collect_step(self, policy: TFPolicy):
+        time_step = self.environment.current_time_step()
+        action_step = policy.action(time_step)
+        next_time_step = self.environment.step(action_step.action)
+        traj = trajectory.from_transition(time_step, action_step, next_time_step)
+
+        # Add trajectory to the replay buffer
+        self.replay_buffer.add_batch(traj)
+
+    def collect_data(self, policy: TFPolicy, steps: int):
+        for _ in range(steps):
+            self.collect_step(policy)
 
     def train(self):
         """
         Executes a custom training loop.
         """
         self.callbacks.on_train_begin()
+
+        if self.epoch == 0:
+            # Evaluate the agent's policy once before training.
+            self.callbacks.on_epoch_begin()
+            self.callbacks.on_epoch_end(0, self.get_evaluation_stats())
+            self.epoch += 1
+
         while self.epoch < self.num_epochs:
-            self.model.compiled_metrics.reset_state()
             self.callbacks.on_epoch_begin(self.epoch)
 
-            self.epoch = self.epoch + 1
-            train_steps = self.train_ds.take(self.batches_per_epoch)
-            epoch_sum_loss = tf.zeros(1)
-            for step, (inputs, targets) in enumerate(train_steps):
+            self.environment.reset()
+            for step in range(self.train_steps_per_epoch):
                 self.callbacks.on_train_batch_begin(step)
-
-                logs = self.update_model(inputs, targets)
-                epoch_sum_loss += logs['loss']
-                logs['loss'] = epoch_sum_loss / (step + 1)
-
+                logs = self.training_step()
+                logs['iteration'] = self.epoch * self.train_steps_per_epoch + step
                 self.callbacks.on_train_batch_end(step, logs)
-            epoch_logs = logs
 
-            if self.val_steps and self.epoch % self.val_freq == 0:
-                val_logs = self.model.evaluate(
-                    self.val_ds,
-                    steps=self.val_steps,
-                    callbacks=self.callbacks,
-                    return_dict=True,
-                    _use_cached_eval_dataset=True)
-                val_logs = {'val_' + name: val for name, val in val_logs.items()}
-                epoch_logs.update(val_logs)
+            epoch_logs = {
+                'epoch': self.epoch,
+                **self.get_evaluation_stats()
+            }
+
+            try:
+                video_file = f'{self.videos_dir}/eval_video_step_{self.epoch}.mp4'
+                create_policy_eval_video(
+                    self.agent.policy, self.eval_env, 
+                    max_steps=self.eval_steps, filename=video_file
+                )
+                print(f'Evaluation video saved to: {video_file}')
+                epoch_logs['eval_video'] = wandb.Video(video_file, format='mp4')
+
+            except Exception as e:
+                print('Failed to create evaluation video:', e)
 
             self.callbacks.on_epoch_end(self.epoch, epoch_logs)
+            self.epoch += 1
 
         self.callbacks.on_train_end()
-        self.history = self.model.history.history
 
-    def plot_loss_curve(self, savefig=True, showfig=False, figsize=(12, 7)):
-        fig, ax = plt.subplots(figsize=figsize)
-        df = pd.DataFrame([
-            {'epoch': epoch, 'loss': loss, 'val_loss': val_loss}
-            for epoch, (loss, val_loss) in enumerate(zip(self.history['loss'],
-                                                         self.history['val_loss']))
-        ])
-        sns.lineplot(data=df, x='epoch', y='loss', ax=ax, label='Train Loss')
-        sns.lineplot(data=df, x='epoch', y='val_loss', ax=ax, label='Validation Loss')
-
-        wandb.log({'loss_fig': plt})
-
-        if savefig:
-            plt.savefig(f'{self.run_dir}/loss_fig.png')
-        if showfig:
-            plt.show()
-
-    def analyse(self):
-        self.plot_loss_curve(savefig=True, showfig=False)
-
-        if self.custom_analyse:
-            self.custom_analyse(self)
-
-        if self.model_type == 'classifier':
-            self._classification_analysis()
-        elif self.model_type == 'autoencoder':
-            self._autoencoder_analysis()
-
-    def _classification_analysis(self):
-        pass
-
-    def _autoencoder_analysis(self):
-        pass
-
-    def _pre_execute_setup(self):
+    def _setup_callbacks(self):
         self.wandb_run = wandb.init(project=self.wandb_project,
                                     entity=self.wandb_entity,
                                     config=self.get_config())
@@ -235,27 +285,52 @@ class TrainingRun:
             steps=self.batches_per_epoch
         )
 
+    def _pre_execute_setup(self):
+        self._setup_callbacks()
+
+        self.agent.train = common.function(self.agent.train)
+
+        # Reset the train step
+        self.agent.train_step_counter.assign(0)
+
+        self.collect_data(self.random_policy, self.initial_collect_steps)
+
+        self.experience_dataset = self.replay_buffer.as_dataset(
+            num_parallel_calls=3,
+            sample_batch_size=self.batch_size,
+            num_steps=2
+        ).prefetch(3)
+
+        self.experience_iterator = iter(self.experience_dataset)
+
+        self.epoch = 0
+
     def execute(self):
         self._pre_execute_setup()
 
         try:
             self.train()
-            if self.do_analysis:
-                self.analyse()
             self.wandb_run.finish()
             print('Run Complete.')
         except KeyboardInterrupt:
             print('Run Interrupted!')
 
         self.save()
-        return self.history
+        return self.get_history()
+
+    def get_history(self) -> Dict[str, list]:
+        """
+        Returns:
+            The training history as a dictionary mapping metric
+            names to lists of historical values.
+        """
+        return self.model.history.history
 
     def save(self):
 
         config = {
-            'ds_info': self.ds_info,
             'run_config': self.get_config(),
-            'history': self._clean_for_json(self.history)
+            'history': self._clean_for_json(self.get_history())
         }
 
         with open(f'{self.run_dir}/config.json', mode='w') as f:
