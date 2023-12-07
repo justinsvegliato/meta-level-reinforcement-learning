6ef083057e9068b84893f80d5a510b78caa130c5
Dylan Cope <dylanr.cope@gmail.com>
2023-12-07 07:23:52 -0800

    changes to run scripts

--- scripts/run_meta_training_ablate_struc.sh
+++ scripts/run_meta_training_ablate_struc.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=$1
 python -m rlts.train.procgen_meta \
       --max_tree_size 256 --pretrained_percentile 0.1 --num_iterations 1000 \
       --collect_steps 4096 --n_collect_envs 64 --train_batch_size 64 --gpus $1 \
-      --ablate_struc_tokenise
+      --ablate_state_tokenise
--- scripts/run_meta_training_batch.sh
+++ scripts/run_meta_training_batch.sh
@@ -5,6 +5,6 @@ do
    # echo $i
    # srun bash -c "systemctl --user enable docker.service && systemctl --user start docker.service;sleep 1;docker run --gpus all --name rlts$1 chai/rlts:latest echo "hello from container $1 on " `hostname`"
    # srun bash -c "./run_meta_training.sh $i"
-   docker run --detach --rm -v $(pwd):/tf/ --workdir /tf --gpus all --name rlts$i chai/rlts:latest python -m rlts.train.procgen_meta \
-      --max_tree_size 256 --pretrained_percentile 0.1 --num_iterations 1000 --collect_steps 4096 --n_collect_envs 64 --train_batch_size 64 --gpus $i
+   docker run --detach --rm -v $(pwd):/tf/ --workdir /tf --gpus all --name rlts-ablate-state chai/rlts:latest python -m rlts.train.procgen_meta \
+      --max_tree_size 256 --pretrained_percentile 0.1 --num_iterations 1000 --collect_steps 4096 --n_collect_envs 64 --train_batch_size 64 --gpus 0 --ablate_state_tokenise
 done
